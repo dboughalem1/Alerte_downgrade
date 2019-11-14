b92fd2533b22b6a1f6c36fb59ae3bd61ffb478d2
Boughalem Dounia <boughalemdounia@MacBook-Pro-de-Boughalem.local>
2019-11-14 15:23:32 +0100

    Ajout fichiers htmls pour l'accueil et la summary

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from app import app
 import pandas as pd
 from flask_caching import Cache
-
+from flask import render_template
 
 
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
@@ -9,12 +9,11 @@ cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 @app.route("/")
 @cache.cached(timeout=50)
-def acceuil():
-    #return render_template('Acceuil.html')
-     return "hello its working !!!!!"
+def accueil():
+    return render_template('accueil.html')
 
 
 @app.route("/Summary")
 @cache.cached(timeout=50)
 def sumup():
-    return render_template('Summary.html')
+    return render_template('sumup.html')
